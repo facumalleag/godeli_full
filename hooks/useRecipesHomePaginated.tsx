999a8772398ebeb2a2c86e050270f251c9287732
gvelizlasteniasoftware <gustavo.veliz@lasteniasoftware.com.ar>
2024-02-19 01:45:14 -0300

    Se modifica homeScreen

--- hooks/useRecipesHomePaginated.tsx
+++ hooks/useRecipesHomePaginated.tsx
@@ -4,26 +4,34 @@ import { Datum, SimpleRecipe } from '../interfaces/RecipesHomeInterface';
 import * as SecureStore from 'expo-secure-store'
 
 
-const useRecipesHomePaginated = () => {
-const [isLoading, setIsLoading] = useState(true)
+  const useRecipesHomePaginated = () => {
+  const [isLoading, setIsLoading] = useState(true)
   const [simpleRecipesList, setSimpleRecipesList] = useState<SimpleRecipe[]>([])
-
+  const [page, setPage] = useState(0);
+  
   useEffect(() => {
     getRecipes()
   }, [])
 
-  const nextPageUrl = useRef("http://godeli.mooo.com:3000/api/v1/recipes?limit=10&puntaje=5")
-
   const getRecipes = async () => {
     setIsLoading(true);
+    let recetas = "http://godeli.mooo.com:3000/api/v1/recipes?limit=15&offset="+page;
     
     const clave = await SecureStore.getItemAsync('access_token');
-    const resp = await recipesApi.get(nextPageUrl.current, {
+    const resp = await recipesApi.get(recetas, {
       headers: {
         Authorization: `Bearer ${clave}`
       }
     })
-    mapSimpleRecipeList(resp.data.data)
+
+    if(resp.data.data.length === 0){
+        console.log('No hay más recetas');
+        setIsLoading(true);
+        return; 
+    }else{
+      mapSimpleRecipeList(resp.data.data)
+      setPage(page + 15);
+    }
 
   }
 
