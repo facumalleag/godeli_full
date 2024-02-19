70e2d8d0c121c6bc867cdb31c86adc1c9ec5b2b4
caroj <caroj@lacaja.com.ar>
2024-02-19 19:12:35 -0300

    Se sube alerta de error de internet

--- app/Login.tsx
+++ app/Login.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { View, ImageBackground, Image, Text, StyleSheet } from 'react-native';
+import { View, ImageBackground, Image, Text, StyleSheet, Alert } from 'react-native';
 import NetworkController from '../controller/NetworkController';
 import { styles } from '../theme/LandingStyle';
 import InternetAlert from '../components/InternetAlert';
@@ -11,6 +11,9 @@ import {
 } from "@react-native-google-signin/google-signin";
 import axios from "axios";
 import * as SecureStore from 'expo-secure-store';
+import NetInfo from '@react-native-community/netinfo';
+import { addEventListener } from "@react-native-community/netinfo";
+import RNRestart from 'react-native-restart';
 
 
 const configureGoogleSignIn = () => {
@@ -69,6 +72,23 @@ export default function Login() {
       setError(e);
     }
   };
+
+  const unsubscribe = NetInfo.addEventListener(state => {
+    if (state.isConnected === false) {
+      Alert.alert('No Internet!', 'Please reconnect!', [
+        {
+          text: 'Reload App',
+          onPress: () => RNRestart.restart(),
+        },
+      ]);
+    } else if (state.isConnected === true) {
+      console.log('Connected');
+    }
+  });
+
+  useEffect(() => {
+    unsubscribe();
+  });
 
   useEffect(() => {
     const checkConnection = async () => {
