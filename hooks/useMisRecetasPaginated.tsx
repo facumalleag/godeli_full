9ee2dd45b50ce6bcb4fb0f44fc41c9c1c65bc69a
gvelizlasteniasoftware <gustavo.veliz@lasteniasoftware.com.ar>
2024-02-20 01:02:41 -0300

    Se corrije visulizazion de retas propias

--- hooks/useMisRecetasPaginated.tsx
+++ hooks/useMisRecetasPaginated.tsx
@@ -14,7 +14,7 @@ import { Datum } from '../interfaces/MisRecetasInterface'
 
   const getMisRecetas = async () => {
     setIsLoading(true);
-    let misRecetas = "http://godeli.mooo.com:3000/api/v1//recipes/?user=me"
+    let misRecetas = "http://godeli.mooo.com:3000/api/v1/recipes/?user=me"
     
     const clave = await SecureStore.getItemAsync('access_token');
     const resp = await misRecetasApi.get(misRecetas, {
